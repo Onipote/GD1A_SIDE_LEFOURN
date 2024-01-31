644025354723f1346f55c9b0116d1aa335514363
Onipote <148326986+Onipote@users.noreply.github.com>
2024-01-31 12:33:14 +0100

    Zone Eau (en cours)

--- Assets/ControlsCharacter.cs
+++ Assets/ControlsCharacter.cs
@@ -18,23 +18,27 @@ public class ControlsCharacter : MonoBehaviour
     [SerializeField] private Rigidbody2D rgbd;
     [SerializeField] private BoxCollider2D boxCharacter;
     private bool isTouchingLayers;
+    private int characterMask;
     private int groundMask;
+    private int waterMask;
     [SerializeField] private LayerMask Traps;
 
-    //Barre de vie (pas fini)
+    /*//Barre de vie (pas fini)
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int currentHealth;
-    private HealthBar healthBar;
+    private HealthBar healthBar;*/
 
     Vector3 startingPosition;
 
     void Start()
     {
         startingPosition = gameObject.transform.position;
+        characterMask = LayerMask.GetMask("Character");
         groundMask = LayerMask.GetMask("Ground");
+        waterMask = LayerMask.GetMask("Water");
 
-        currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        /*currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);*/
     }
     void Update() // Update is called once per frame
     {
@@ -51,19 +55,41 @@ public class ControlsCharacter : MonoBehaviour
         {
             rgbd.AddForce(Vector2.up * 5, ForceMode2D.Impulse);
         }
+
+        //Altération déplacement (résistance de l'eau)
+        if (boxCharacter.IsTouchingLayers(waterMask))
+        {
+            rgbd.mass = 4;
+            Debug.Log("touche l'eau");
+        }
+        if (boxCharacter.IsTouchingLayers(groundMask))
+        {
+            rgbd.mass = 1;
+            Debug.Log("touche le sol");
+        }
+        //Obstacle causant la mort du joueur
         if (boxCharacter.IsTouchingLayers(Traps))
         {
             rgbd.transform.position = startingPosition;
         }
-        //Test TakeDamage
+
+        /*//Test TakeDamage
         if (Input.GetKeyDown(KeyCode.F))
         {
             TakeDamage(25);
+        }*/
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.IsTouchingLayers(characterMask))
+        {
+            rgbd.velocity = new Vector3(1, 0, 0);
+            Debug.Log("ralentissement");
         }
     }
-    void TakeDamage(int damage)
+    /*void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
-    }
+    }*/
 }
